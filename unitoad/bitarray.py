e3cc4d01d2b8bbdf509afb97a1359813a424944a
Anson Mansfield <anson.mansfield@gmail.com>
2021-11-28 13:10:19 -0500

    annotation cleanup

--- unitoad/bitarray.py
+++ unitoad/bitarray.py
@@ -63,7 +63,7 @@ class BitArray:
         mask = self.order(bit)
         return bool(self.data[idx] & mask)
 
-    def _getitem_slice(self, index: slice) -> 'BitArray':
+    def _getitem_slice(self, index: slice) -> BitArray:
         indices = range(len(self))[index]
         space = ceildiv(len(indices),8)
         result = BitArray(bytearray(b'\0'*space), order=self.order, _len=len(indices))
@@ -94,7 +94,7 @@ class BitArray:
         for i, v in zip(indices, values):
             self[i] = v
     
-    # def __iter__(self) -> Generator[bool, None, None]:
+    # def __iter__(self) -> Generator[bool, None, None]: # python provides this by default from the len and getitem methods
     #     for i in range(len(self)):
     #         yield self[i]
     
@@ -122,7 +122,7 @@ class BitArray:
         else:
             return self >> 8*(byte+1) << 8 - bits
 
-    def _iop(self, other: 'BitArray' | Iterable[SupportsIndex] | SupportsBytes, op: Callable[[int,int],int], slack: bool) -> BitArray:
+    def _iop(self, other: BitArray | Iterable[SupportsIndex] | SupportsBytes, op: Callable[[int,int],int], slack: bool) -> BitArray:
         if isinstance(other, BitArray):
             other._set_slack_bits(slack)
             it = enumerate(zip(self.data, other.data))
@@ -133,7 +133,7 @@ class BitArray:
             self.data[i] = op(a, b)
         return self
 
-    def _op(self, other: 'BitArray', op: Callable[[Any,Any],Any], slack: bool) -> BitArray:
+    def _op(self, other: BitArray, op: Callable[[Any,Any],Any], slack: bool) -> BitArray:
         return copy.copy(self)._iop(other, op, slack)
 
     __iand__ = partialmethod(_iop, op=operator.and_, slack=True)
@@ -143,7 +143,7 @@ class BitArray:
     __xor__ = partialmethod(_op, op=operator.xor, slack=False)
     __or__ = partialmethod(_op, op=operator.or_, slack=False)
 
-    def __iadd__(self, other: 'BitArray' | Iterable[SupportsIndex] | SupportsBytes) -> BitArray:
+    def __iadd__(self, other: BitArray | Iterable[SupportsIndex] | SupportsBytes) -> BitArray:
         slack = self._num_slack()
         self >>= slack
         if isinstance(other, BitArray):
